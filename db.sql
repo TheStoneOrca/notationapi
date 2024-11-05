6b12afa3caa223349197fb3e09f1e723f6c342b4
TheStoneOrca <144631439+TheStoneOrca@users.noreply.github.com>
2024-11-05 02:22:48 -0500

    commit

---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+    userID Serial Primary Key,
+    username TEXT,
+    password TEXT,
+    email TEXT
+);
---
+++ server.js
@@ -0,0 +1,63 @@
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+const db = new pg.Client({
+  connectionString:
+    "postgresql://todolist_owner:EZzU0QWn1euc@ep-wandering-fire-a5u4bxp2.us-east-2.aws.neon.tech/notation?sslmode=require",
+});
+
+db.connect().then(() => {
+  console.log("Connected to Database!");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post("/signup", async (req, res) => {
+  try {
+    const data = req.body;
+    const checkUserName = await db.query(
+      "SELECT * FROM users WHERE username = $1",
+      [data.username]
+    );
+    const checkEmail = await db.query("SELECT * FROM users WHERE email = $1", [
+      data.email,
+    ]);
+
+    if (checkUserName.rows.length > 0 || checkEmail.rows.length > 0) {
+      console.log("400");
+      return res.sendStatus(401);
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const userData = await db.query(
+      "INSERT INTO users(username,password,email) VALUES($1,$2,$3) RETURNING userid,username,email",
+      [data.username, hashedPassword, data.email]
+    );
+
+    const userDataJWT = jwt.sign(userData.rows[0], process.env.JWTToken);
+    console.log("200");
+    return res.json({ JWT: userDataJWT });
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(500);
+  }
+});
+
+app.post("/signin", (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}!`);
+});
